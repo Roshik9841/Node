a5b29d4c721443b5d72766efcc0a0dc54a4d0f88
I am Roshik <Roshik9841@gmail.com>
2025-11-19 19:24:28 +0545

    Get request

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
  
  
- const http = require('http');
+//  const http = require('http');
 
-http.createServer(function(req,res){
+// http.createServer(function(req,res){
 
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write("Welcome back");
-    res.end();
-}).listen(8080)
+//     res.writeHead(200,{'Content-Type':'text/html'});
+//     res.write("Welcome back");
+//     res.end();
+// }).listen(8080)
+
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Express js");
+})
+
+
+app.get('/alien',(req,res)=>{
+    res.send("Hi I am alien");
+})
+
+app.listen(9000,(req,res)=>{
+    console.log("Server is running on port 9000");
+});
