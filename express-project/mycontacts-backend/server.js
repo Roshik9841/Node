2a316c42d73ae436ebe0eee8544eaa9450f48b32
I am Roshik <Roshik9841@gmail.com>
2025-11-20 14:56:42 +0545

    Api created for get

--- express-project/mycontacts-backend/server.js
+++ express-project/mycontacts-backend/server.js
@@ -4,7 +4,9 @@ const dotenv = require("dotenv").config();
 const app =express();
 const port = process.env.PORT || 5000;
 
-
+app.get('/api/contacts',(req,res)=>{
+    res.status(201).send("Get all contacts");
+})
 
 app.listen(port,()=>{
     console.log("Server running on port"+ port);
