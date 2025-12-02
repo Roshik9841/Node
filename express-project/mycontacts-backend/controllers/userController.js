1a87ca8a401706393b5310d89e416626eff7ee4c
I am Roshik <Roshik9841@gmail.com>
2025-12-02 18:32:42 +0545

    JWT Access Token & User LogiN

--- express-project/mycontacts-backend/controllers/userController.js
+++ express-project/mycontacts-backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require('../models/userModel');
 
 // @descRegister a user
@@ -34,6 +35,28 @@ const registerUser = asyncHandler(async (req, res) => {
 //@router POST /api/users/login
 //@access private
 const loginUser = asyncHandler(async (req, res) => {
+    const {email,password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatoyr");
+    }
+
+    const user = await User.findOne({email});
+    //compare password with hashed password
+    if(user && (await bcrypt.compare(password,user.password))){
+        const accessToken = jwt.sign({
+            user:{
+                username:user.username,
+                email:user.email,
+                id:user.id,
+            },
+        },process.env.ACCCESS_TOKEN_SECRET,{expiresIn:"1m"});
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401);
+        throw new Error("Email or password is not valid");
+    }
+
      res.json({message:"Login the user"})
 });
 
