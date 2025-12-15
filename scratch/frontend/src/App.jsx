25cfe0d52baae2d84de89d4569b748735aa1c46d
I am Roshik <Roshik9841@gmail.com>
2025-12-15 15:33:50 +0545

    SetError

--- scratch/frontend/src/App.jsx
+++ scratch/frontend/src/App.jsx
@@ -5,41 +5,67 @@ function App() {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors, isSubmitting },
   } = useForm();
 
-  function onSubmit(data) {
-    console.log(data);
+  async function onSubmit(data) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      console.log(data);
+      throw new Error();
+    } catch (error) {
+      setError("root", { message: "This is incorrect" });
+    }
   }
+
   return (
-    <div className=" min-h-screen flex justify-center items-center  bg-gray-100">
-      <div className=" w-full max-w-sm bg-white shadow-lg p-6">
-        <form onSubmit={handleSubmit(onSubmit)} className="  space-y-6">
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <div className="w-full max-w-sm bg-white shadow-lg p-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <input
             {...register("email", {
-              required: "Email must be in @ and required",
-              pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "Invalid email format",
+              },
             })}
             type="text"
             placeholder="Email"
-            className="w-full border rounded-lg"
-         
+            className="w-full border rounded-lg p-2"
           />
-          {errors.email && <div className="text-red-300">{errors.email.message}</div>}
+          {errors.email && (
+            <div className="text-red-400">{errors.email.message}</div>
+          )}
+
           <input
-            {...register("password", { required: "Password is required", minLength: 6 })}
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
             type="password"
-            placeholder="password"
-            className="w-full border rounded-lg"
-        
+            placeholder="Password"
+            className="w-full border rounded-lg p-2"
           />
-          {errors.password && <div className="text-red-300">{errors.password.message}</div>}
+          {errors.password && (
+            <div className="text-red-400">{errors.password.message}</div>
+          )}
+
           <button
+            disabled={isSubmitting}
             type="submit"
-            className="bg-red-200 rounded-2xl w-1/2 hover:bg-blue-400 p-2 text-white"
+            className="bg-red-200 rounded-2xl w-1/2 mx-auto block hover:bg-blue-400 p-2 text-white"
           >
-            {isSubmitting?"Submitting":"Submit"}
+            {isSubmitting ? "Loading..." : "Submit"}
           </button>
+          {errors.root && (
+            <div className="text-red-400">{errors.root.message}</div>
+          )}
         </form>
       </div>
     </div>
