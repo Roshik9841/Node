d6518c3430c0bad71cdcec7464ef9b9f9ac40815
I am Roshik <Roshik9841@gmail.com>
2025-12-15 18:57:42 +0545

    Login page using shadcn

--- scratch/frontends/src/App.tsx
+++ scratch/frontends/src/App.tsx
@@ -1,11 +1,17 @@
 import { Button } from "./components/ui/button";
 import React from "react";
+import {Login } from "./Pages/Login"
 
  const App = ()=>{
    return(
-    <>
+ 
+    <div className="min-h-screen max-w-5xl mx-auto p-5 ">
+
+    <Login/>
+
     <Button>Submit</Button>
-    </>
+    </div>
+   
    ) 
 }
 export default App;
