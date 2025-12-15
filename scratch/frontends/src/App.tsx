c7128aba4c4bb067ec3c4caed6a82b760ffa0013
I am Roshik <Roshik9841@gmail.com>
2025-12-15 19:37:31 +0545

    Toggle theme

--- scratch/frontends/src/App.tsx
+++ scratch/frontends/src/App.tsx
@@ -1,15 +1,20 @@
 import { Button } from "./components/ui/button";
 import React from "react";
+import { ThemeProvider } from "./components/theme-provider"
+ import { ModeToggle } from "./components/mode-toggle";
 import {Login } from "./Pages/Login"
 
  const App = ()=>{
    return(
  
     <div className="min-h-screen max-w-5xl mx-auto p-5 ">
+ <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+  <ModeToggle/>
+         <Login/>
+    </ThemeProvider>
+ 
 
-    <Login/>
-
-    <Button>Submit</Button>
+    {/* <Button>Submit</Button> */}
     </div>
    
    ) 
