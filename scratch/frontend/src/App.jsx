7e162e98a26f1ca18819651c99eda011c5fdeb52
I am Roshik <Roshik9841@gmail.com>
2025-12-15 15:19:51 +0545

    useForm

--- scratch/frontend/src/App.jsx
+++ scratch/frontend/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
 
+  function onSubmit(data) {
+    console.log(data);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" min-h-screen flex justify-center items-center  bg-gray-100">
+      <div className=" w-full max-w-sm bg-white shadow-lg p-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="  space-y-6">
+          <input
+            {...register("email", {
+              required: "Email must be in @ and required",
+              pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            })}
+            type="text"
+            placeholder="Email"
+            className="w-full border rounded-lg"
+         
+          />
+          {errors.email && <div className="text-red-300">{errors.email.message}</div>}
+          <input
+            {...register("password", { required: "Password is required", minLength: 6 })}
+            type="password"
+            placeholder="password"
+            className="w-full border rounded-lg"
+        
+          />
+          {errors.password && <div className="text-red-300">{errors.password.message}</div>}
+          <button
+            type="submit"
+            className="bg-red-200 rounded-2xl w-1/2 hover:bg-blue-400 p-2 text-white"
+          >
+            {isSubmitting?"Submitting":"Submit"}
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
