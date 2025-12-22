0adef24f0404d84c59f6082a2ea0018a7f21a05f
I am Roshik <Roshik9841@gmail.com>
2025-12-22 14:39:07 +0545

    object literal annotation

--- scratch/TS/variable.ts
+++ scratch/TS/variable.ts
@@ -1,1 +1,9 @@
-const colors: string[] = ['red','blue','green'];
+const colors: string[] = ["red", "blue", "green"];
+
+class Car {}
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
