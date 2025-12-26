d8e79b09efd5aef2f724ed7923e5f88cf2c276fc
I am Roshik <Roshik9841@gmail.com>
2025-12-26 14:31:30 +0545

    Repository, service an controller of users

--- scratch/mycv/src/users/users.controller.ts
+++ scratch/mycv/src/users/users.controller.ts
@@ -1,4 +1,17 @@
 import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userServices: UsersService) {}
+  @Get()
+  findAllUsers() {
+    return this.userServices.findAllUsers();
+  }
+  @Post()
+  createUser(@Body() body: UserDto) {
+    return this.userServices.createUser(body);
+  }
+}
--- scratch/mycv/src/users/users.service.ts
+++ scratch/mycv/src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { UserDto } from './dto/user.dto';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+  async findAllUsers(): Promise<User[]> {
+    const users = await this.userRepository.find();
+    return users;
+  }
+  createUser(body: UserDto): Promise<User> {
+    const users = this.userRepository.create(body);
+    return this.userRepository.save(users);
+  }
+}
